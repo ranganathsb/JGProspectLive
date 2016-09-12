d167af72c6dc90b96f83f91b40fe9bd6cd43c5e5
Yogesh Keraliya <kerconsultancy@hotmail.com>
2016-09-13 01:28:39 +0530

    Added ICE scripts for version control.

---
+++ JG_Prospect.Common/modal/TaskWorkSpecification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JG_Prospect.Common.modal
+{
+    public class TaskWorkSpecification
+    {
+        public Int64 Id;
+        public Int64 TaskId;
+        public Int32 UserId;
+        public bool IsInstallUser;
+        public Int16 Status;
+        public string Content;
+        public DateTime DateCreated;
+        public TaskWorkSpecificationVersions TaskWorkSpecificationVersions;
+    }
+}
---
+++ JG_Prospect.Common/modal/TaskWorkSpecificationVersions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JG_Prospect.Common.modal
+{
+    public class TaskWorkSpecificationVersions
+    {
+        public Int64 Id;
+        public Int64 TaskWorkSpecificationId;
+        public Int32 UserId;
+        public bool IsInstallUser;
+        public string Content;
+        public DateTime DateCreated;
+        public TaskWorkSpecification TaskWorkSpecification;
+    }
+}
