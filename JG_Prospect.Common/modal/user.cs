b197b9f7c513ae18331f9b48c2553dc4e54d15b4
KER Consultancy <kerconsultancy@hotmail.com>
2016-09-28 11:34:26 +0530

    Revert "removing phone2 as its already added"
    This reverts commit 761b063e376dd39cdb9c291cdf1aa741ae370b0f.

--- JG_Prospect.Common/modal/user.cs
+++ JG_Prospect.Common/modal/user.cs
@@ -183,12 +183,7 @@ namespace JG_Prospect.Common.modal
     public class user1
     {
         public int Id;
-        public string Email;
-
-        public string Email2;//added by Ratnakar
-        public string Notes;//added by Ratnakar
-       // public string Phone2;//added by Ratnakar
-
+        public string Email; 
         public string Designation;
         public string usertype;
         public string status;
