7a941d14604c707f1ca1fa064ceb926d64f6406b
Yogesh Keraliya <kerconsultancy@hotmail.com>
2016-10-04 08:38:15 +0530

    Added database scripts folder.

---
+++ JG_Prospect.web/Database Scripts/scripts oct v0.1.sql
@@ -0,0 +1,437 @@
+USE [JGBS]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Yogesh Keraliya
+-- Create date: 07/15/2016
+-- Description:	Update task priority
+-- =============================================
+CREATE PROCEDURE [dbo].[usp_UpdateTaskPriority] 
+(	
+	@TaskId int , 	
+	@TaskPriority tinyint
+)
+AS
+BEGIN
+	
+	UPDATE tblTask
+	SET TaskPriority = @TaskPriority
+	WHERE TaskId = @TaskId
+
+END
+GO
+
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Yogesh Keraliya
+-- Create date: 04/07/2016
+-- Description:	Load all details of task for edit.
+-- =============================================
+-- usp_GetTaskDetails 115
+ALTER PROCEDURE [dbo].[usp_GetTaskDetails] 
+(
+	@TaskId int 
+)	  
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+
+	-- task manager detail
+	DECLARE @AssigningUser varchar(50) = NULL
+
+	SELECT @AssigningUser = Users.[Username] 
+	FROM 
+		tblTask AS Task 
+		INNER JOIN [dbo].[tblUsers] AS Users  ON Task.[CreatedBy] = Users.Id
+	WHERE TaskId = @TaskId
+
+	IF(@AssigningUser IS NULL)
+	BEGIN
+		SELECT @AssigningUser = Users.FristName + ' ' + Users.LastName 
+		FROM 
+			tblTask AS Task 
+			INNER JOIN [dbo].[tblInstallUsers] AS Users  ON Task.[CreatedBy] = Users.Id
+		WHERE TaskId = @TaskId
+	END
+
+	-- task's main details
+	SELECT Title, [Description], [Status], DueDate,Tasks.[Hours], Tasks.CreatedOn, Tasks.TaskPriority,
+		   Tasks.InstallId, Tasks.CreatedBy, @AssigningUser AS AssigningManager ,Tasks.TaskType,
+		   STUFF
+			(
+				(SELECT  CAST(', ' + ttuf.[Attachment] + '@' + ttuf.[AttachmentOriginal] as VARCHAR(max)) AS attachment
+				FROM dbo.tblTaskUserFiles ttuf
+				WHERE ttuf.TaskId = Tasks.TaskId
+				FOR XML PATH(''), TYPE).value('.','NVARCHAR(MAX)')
+				,1
+				,2
+				,' '
+			) AS attachment
+	FROM tblTask AS Tasks
+	WHERE Tasks.TaskId = @TaskId
+
+	-- task's designation details
+	SELECT Designation
+	FROM tblTaskDesignations
+	WHERE (TaskId = @TaskId)
+
+	-- task's assigned users
+	SELECT UserId, TaskId
+	FROM tblTaskAssignedUsers
+	WHERE (TaskId = @TaskId)
+
+	-- task's notes and attachment information.
+	SELECT	TaskUsers.Id,TaskUsers.UserId, TaskUsers.UserType, TaskUsers.Notes, TaskUsers.UserAcceptance, TaskUsers.UpdatedOn, 
+		    TaskUsers.[Status], TaskUsers.TaskId, tblInstallUsers.FristName,TaskUsers.UserFirstName, tblInstallUsers.Designation,
+			(SELECT COUNT(ttuf.[Id]) FROM dbo.tblTaskUserFiles ttuf WHERE ttuf.[TaskUpdateID] = TaskUsers.Id) AS AttachmentCount,
+			dbo.UDF_GetTaskUpdateAttachments(TaskUsers.Id) AS attachments
+	FROM    
+		tblTaskUser AS TaskUsers 
+		LEFT OUTER JOIN tblInstallUsers ON TaskUsers.UserId = tblInstallUsers.Id
+	WHERE (TaskUsers.TaskId = @TaskId) 
+
+	-- sub tasks
+	SELECT Tasks.TaskId, Title, [Description], Tasks.[Status], DueDate,Tasks.[Hours], Tasks.CreatedOn, Tasks.TaskPriority,
+		   Tasks.InstallId, Tasks.CreatedBy, @AssigningUser AS AssigningManager , UsersMaster.FristName,
+		   Tasks.TaskType,Tasks.TaskPriority,
+		   STUFF
+			(
+				(SELECT  CAST(', ' + ttuf.[Attachment] + '@' + ttuf.[AttachmentOriginal] as VARCHAR(max)) AS attachment
+				FROM dbo.tblTaskUserFiles ttuf
+				WHERE ttuf.TaskId = Tasks.TaskId
+				FOR XML PATH(''), TYPE).value('.','NVARCHAR(MAX)')
+				,1
+				,2
+				,' '
+			) AS attachment
+	FROM 
+		tblTask AS Tasks LEFT OUTER JOIN
+        tblTaskAssignedUsers AS TaskUsers ON Tasks.TaskId = TaskUsers.TaskId LEFT OUTER JOIN
+        tblInstallUsers AS UsersMaster ON TaskUsers.UserId = UsersMaster.Id --LEFT OUTER JOIN
+		--tblTaskDesignations AS TaskDesignation ON Tasks.TaskId = TaskDesignation.TaskId
+	WHERE Tasks.ParentTaskId = @TaskId
+    
+	-- main task attachments
+	SELECT 
+		CAST(
+				tuf.[Attachment] + '@' + tuf.[AttachmentOriginal] 
+				AS VARCHAR(MAX)
+			) AS attachment,
+		ISNULL(u.FirstName,iu.FristName) AS FirstName
+	FROM dbo.tblTaskUserFiles tuf
+			LEFT JOIN tblUsers u ON tuf.UserId = u.Id --AND tuf.UserType = u.Usertype
+			LEFT JOIN tblInstallUsers iu ON tuf.UserId = iu.Id --AND tuf.UserType = u.UserType
+	WHERE tuf.TaskId = @TaskId
+
+END
+GO
+
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Yogesh Keraliya
+-- Create date: 8/25/16
+-- Description:	This procedure is used to search tasks by different parameters.
+-- =============================================
+ALTER PROCEDURE [dbo].[uspSearchTasks]
+	@Designations	VARCHAR(4000) = '0',
+	@UserId			INT = NULL,
+	@Status			TINYINT = NULL,
+	@CreatedFrom	DATETIME = NULL,
+	@CreatedTo		DATETIME = NULL,
+	@SearchTerm		VARCHAR(250) = NULL,
+	@SortExpression	VARCHAR(250) = 'CreatedOn DESC',
+	@PageIndex		INT = 0,
+	@PageSize		INT = 10
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	SET @PageIndex = @PageIndex + 1
+
+	;WITH Tasklist
+	AS
+	(	
+		SELECT 
+			--TaskUserMatch.IsMatch AS TaskUserMatch,
+			--TaskUserRequestsMatch.IsMatch AS TaskUserRequestsMatch,
+			--TaskDesignationMatch.IsMatch AS TaskDesignationMatch,
+			Tasks.TaskId, 
+			Tasks.Title, 
+			Tasks.InstallId, 
+			Tasks.[Status], 
+			Tasks.[CreatedOn],
+			Tasks.[DueDate], 
+			Tasks.IsDeleted,
+			Tasks.CreatedBy,
+			Tasks.TaskPriority,
+			STUFF
+			(
+				(SELECT  CAST(', ' + td.Designation as VARCHAR) AS Designation
+				FROM tblTaskDesignations td
+				WHERE td.TaskId = Tasks.TaskId
+				FOR XML PATH(''), TYPE).value('.','NVARCHAR(MAX)')
+				,1
+				,2
+				,' '
+			) AS TaskDesignations,
+			STUFF
+			(
+				(SELECT  CAST(', ' + u.FristName as VARCHAR) AS Name
+				FROM tblTaskAssignedUsers tu
+					INNER JOIN tblInstallUsers u ON tu.UserId = u.Id
+				WHERE tu.TaskId = Tasks.TaskId
+				FOR XML PATH(''), TYPE).value('.','NVARCHAR(MAX)')
+				,1
+				,2
+				,' '
+			) AS TaskAssignedUsers,
+			STUFF
+			(
+				(SELECT  CAST(', ' + CAST(tu.UserId AS VARCHAR) + ':' + u.FristName as VARCHAR) AS Name
+				FROM tblTaskAssignmentRequests tu
+					INNER JOIN tblInstallUsers u ON tu.UserId = u.Id
+				WHERE tu.TaskId = Tasks.TaskId
+				FOR XML PATH(''), TYPE).value('.','NVARCHAR(MAX)')
+				,1
+				,2
+				,' '
+			) AS TaskAssignmentRequestUsers
+		FROM          
+			tblTask AS Tasks 
+			OUTER APPLY
+			(
+				SELECT TOP 1 
+						1 AS IsMatch,
+						TaskUsers.UserId AS UserId,
+						UsersMaster.FristName AS FristName
+				FROM tblTaskAssignedUsers TaskUsers
+						LEFT JOIN tblInstallUsers AS UsersMaster ON TaskUsers.UserId = UsersMaster.Id
+				WHERE 
+					TaskUsers.TaskId = Tasks.TaskId AND
+					TaskUsers.[UserId] = ISNULL(@UserId, TaskUsers.[UserId]) AND
+					1 = CASE
+							WHEN @UserId IS NOT NULL THEN 1 -- set true, when user id is provided. so that join will handle record filtering and search term will have no effect on user.
+							WHEN @SearchTerm IS NULL THEN 1 -- set true, when search term is null. so that join will handle record filtering and search term will have no effect on user.
+							WHEN UsersMaster.FristName LIKE '%' + @SearchTerm + '%' THEN 1 -- set true if users with given search terms are available. 
+							ELSE 0
+						END
+			) As TaskUserMatch
+			OUTER APPLY
+			(
+				SELECT TOP 1 
+						1 AS IsMatch,
+						TaskUsers.UserId AS UserId,
+						UsersMaster.FristName AS FristName
+				FROM tblTaskAssignmentRequests TaskUsers
+						LEFT JOIN tblInstallUsers AS UsersMaster ON TaskUsers.UserId = UsersMaster.Id
+				WHERE 
+					TaskUsers.TaskId = Tasks.TaskId AND
+					TaskUsers.[UserId] = ISNULL(@UserId, TaskUsers.[UserId]) AND
+					1 = CASE
+							WHEN @UserId IS NOT NULL THEN 1 -- set true, when user id is provided. so that join will handle record filtering and search term will have no effect on user.
+							WHEN @SearchTerm IS NULL THEN 1 -- set true, when search term is null. so that join will handle record filtering and search term will have no effect on user.
+							WHEN UsersMaster.FristName LIKE '%' + @SearchTerm + '%' THEN 1 -- set true if users with given search terms are available. 
+							ELSE 0
+						END
+			) As TaskUserRequestsMatch
+			OUTER APPLY
+			(
+				SELECT TOP 1 
+						CASE
+						WHEN @SearchTerm IS NULL THEN
+							CASE
+								WHEN @Designations = '0' THEN 1
+								WHEN EXISTS (SELECT ss.Item  FROM dbo.SplitString(@Designations,',') ss WHERE ss.Item = TaskDesignations.Designation) THEN 1
+								ELSE 0 
+							END
+						ELSE 
+							CASE
+								WHEN @Designations = '0' AND TaskDesignations.Designation LIKE '%' + @SearchTerm + '%' THEN 1
+								WHEN (Tasks.[InstallId] LIKE '%' + @SearchTerm + '%'  OR Tasks.[Title] LIKE '%' + @SearchTerm + '%') THEN 1
+								ELSE 0
+							END
+						END AS IsMatch,
+						TaskDesignations.Designation AS Designation
+				FROM tblTaskDesignations AS TaskDesignations
+				WHERE 
+					TaskDesignations.TaskId = Tasks.TaskId AND
+					1 = CASE
+							WHEN @Designations = '0' AND @SearchTerm IS NULL THEN 1 -- set true, when '0' (all designations) is provided with no search term.
+							WHEN @Designations = '0' AND @SearchTerm IS NOT NULL AND TaskDesignations.Designation LIKE '%' + @SearchTerm + '%' THEN 1 -- set true if designations found by search term.
+							WHEN EXISTS (SELECT ss.Item  FROM dbo.SplitString(@Designations,',') ss WHERE ss.Item = TaskDesignations.Designation) THEN 1 -- filter based on provided designations.
+							ELSE 0
+						END
+			)  AS TaskDesignationMatch
+		WHERE
+			Tasks.ParentTaskId IS NULL 
+			AND 
+			1 = CASE 
+					-- filter records only by user, when search term is not provided.
+					WHEN @SearchTerm IS NULL THEN
+						CASE
+							WHEN TaskUserMatch.IsMatch = 1 OR TaskDesignationMatch.IsMatch = 1 THEN 1
+							WHEN TaskUserRequestsMatch.IsMatch = 1 OR TaskDesignationMatch.IsMatch = 1 THEN 1
+							ELSE 0
+						END
+					-- filter records by installid, title, users when search term is provided.
+					ELSE
+						CASE
+							WHEN Tasks.[InstallId] LIKE '%' + @SearchTerm + '%' THEN 1
+							WHEN Tasks.[Title] LIKE '%' + @SearchTerm + '%' THEN 1
+							WHEN TaskUserMatch.IsMatch = 1 THEN 1
+							WHEN TaskUserRequestsMatch.IsMatch = 1 THEN 1
+							ELSE 0
+						END
+				END
+			--AND
+			--1 = CASE 
+			--	WHEN @SearchTerm IS NULL THEN 
+			--		CASE
+			--			WHEN TaskDesignationMatch.IsMatch = 1 THEN 1
+			--			ELSE 0
+			--		END
+			--	ELSE
+			--		CASE
+			--			WHEN Tasks.[InstallId] LIKE '%' + @SearchTerm + '%' THEN 1
+			--			WHEN Tasks.[Title] LIKE '%' + @SearchTerm + '%' THEN 1
+			--			WHEN TaskDesignationMatch.IsMatch = 1 THEN 1
+			--			ELSE 0
+			--		END
+			--END
+			AND
+			Tasks.[Status] = ISNULL(@Status,Tasks.[Status]) 
+			AND
+			CONVERT(VARCHAR,Tasks.[CreatedOn],101)  >= ISNULL(@CreatedFrom,CONVERT(VARCHAR,Tasks.[CreatedOn],101)) AND
+			CONVERT(VARCHAR,Tasks.[CreatedOn],101)  <= ISNULL(@CreatedTo,CONVERT(VARCHAR,Tasks.[CreatedOn],101))
+	),
+
+FinalData AS( 
+	SELECT * ,
+			Row_number() OVER
+			(
+				ORDER BY
+					CASE WHEN @SortExpression = 'UserID DESC' THEN Tasklist.TaskAssignedUsers END DESC,
+					CASE WHEN @SortExpression = 'CreatedOn DESC' THEN Tasklist.CreatedOn END DESC,
+					CASE WHEN @SortExpression = 'Status ASC' THEN Tasklist.[Status] END ASC
+			) AS RowNo
+	FROM Tasklist )
+	
+	SELECT * FROM FinalData 
+	WHERE  
+		RowNo BETWEEN (@PageIndex - 1) * @PageSize + 1 AND 
+		@PageIndex * @PageSize
+
+	SELECT 
+		COUNT(DISTINCT Tasks.TaskId) AS VirtualCount
+	FROM          
+		tblTask AS Tasks 
+		OUTER APPLY
+		(
+			SELECT TOP 1 
+					1 AS IsMatch,
+					TaskUsers.UserId AS UserId,
+					UsersMaster.FristName AS FristName
+			FROM tblTaskAssignedUsers TaskUsers
+					LEFT JOIN tblInstallUsers AS UsersMaster ON TaskUsers.UserId = UsersMaster.Id
+			WHERE 
+				TaskUsers.TaskId = Tasks.TaskId AND
+				TaskUsers.[UserId] = ISNULL(@UserId, TaskUsers.[UserId]) AND
+				1 = CASE
+						WHEN @UserId IS NOT NULL THEN 1 -- set true, when user id is provided. so that join will handle record filtering and search term will have no effect on user.
+						WHEN @SearchTerm IS NULL THEN 1 -- set true, when search term is null. so that join will handle record filtering and search term will have no effect on user.
+						WHEN UsersMaster.FristName LIKE '%' + @SearchTerm + '%' THEN 1 -- set true if users with given search terms are available. 
+						ELSE 0
+					END
+		) As TaskUserMatch
+		OUTER APPLY
+		(
+			SELECT TOP 1 
+					1 AS IsMatch,
+					TaskUsers.UserId AS UserId,
+					UsersMaster.FristName AS FristName
+			FROM tblTaskAssignmentRequests TaskUsers
+					LEFT JOIN tblInstallUsers AS UsersMaster ON TaskUsers.UserId = UsersMaster.Id
+			WHERE 
+				TaskUsers.TaskId = Tasks.TaskId AND
+				TaskUsers.[UserId] = ISNULL(@UserId, TaskUsers.[UserId]) AND
+				1 = CASE
+						WHEN @UserId IS NOT NULL THEN 1 -- set true, when user id is provided. so that join will handle record filtering and search term will have no effect on user.
+						WHEN @SearchTerm IS NULL THEN 1 -- set true, when search term is null. so that join will handle record filtering and search term will have no effect on user.
+						WHEN UsersMaster.FristName LIKE '%' + @SearchTerm + '%' THEN 1 -- set true if users with given search terms are available. 
+						ELSE 0
+					END
+		) As TaskUserRequestsMatch
+		OUTER APPLY
+		(
+			SELECT TOP 1 
+					1 AS IsMatch,
+					TaskDesignations.Designation AS Designation
+			FROM tblTaskDesignations AS TaskDesignations
+			WHERE 
+				TaskDesignations.TaskId = Tasks.TaskId AND
+				1 = CASE
+						WHEN @Designations = '0' AND @SearchTerm IS NULL THEN 1 -- set true, when '0' (all designations) is provided with no search term.
+						WHEN @Designations = '0' AND @SearchTerm IS NOT NULL AND TaskDesignations.Designation LIKE '%' + @SearchTerm + '%' THEN 1 -- set true if designations found by search term.
+						WHEN EXISTS (SELECT ss.Item  FROM dbo.SplitString(@Designations,',') ss WHERE ss.Item = TaskDesignations.Designation) THEN 1 -- filter based on provided designations.
+						ELSE 0
+					END
+		)  AS TaskDesignationMatch
+	WHERE
+		Tasks.ParentTaskId IS NULL 
+		AND 
+		1 = CASE 
+				-- filter records only by user, when search term is not provided.
+				WHEN @SearchTerm IS NULL THEN
+					CASE
+						WHEN TaskUserMatch.IsMatch = 1 OR TaskDesignationMatch.IsMatch = 1 THEN 1
+						WHEN TaskUserRequestsMatch.IsMatch = 1 OR TaskDesignationMatch.IsMatch = 1THEN 1
+						ELSE 0
+					END
+				-- filter records by installid, title, users when search term is provided.
+				ELSE
+					CASE
+						WHEN Tasks.[InstallId] LIKE '%' + @SearchTerm + '%' THEN 1
+						WHEN Tasks.[Title] LIKE '%' + @SearchTerm + '%' THEN 1
+						WHEN TaskUserMatch.IsMatch = 1 THEN 1
+						WHEN TaskUserRequestsMatch.IsMatch = 1 THEN 1
+						ELSE 0
+					END
+			END
+		--AND
+		--1 = CASE 
+		--		WHEN @SearchTerm IS NULL THEN 
+		--			CASE
+		--				WHEN TaskDesignationMatch.IsMatch = 1 THEN 1
+		--				ELSE 0
+		--			END
+		--		ELSE
+		--			CASE
+		--				WHEN Tasks.[InstallId] LIKE '%' + @SearchTerm + '%' THEN 1
+		--				WHEN Tasks.[Title] LIKE '%' + @SearchTerm + '%' THEN 1
+		--				WHEN TaskDesignationMatch.IsMatch = 1 THEN 1
+		--				ELSE 0
+		--			END
+		--	END 
+		AND
+		Tasks.[Status] = ISNULL(@Status,Tasks.[Status]) 
+		AND
+		CONVERT(VARCHAR,Tasks.[CreatedOn],101)  >= ISNULL(@CreatedFrom,CONVERT(VARCHAR,Tasks.[CreatedOn],101)) AND
+		CONVERT(VARCHAR,Tasks.[CreatedOn],101)  <= ISNULL(@CreatedTo,CONVERT(VARCHAR,Tasks.[CreatedOn],101))
+
+END
+GO
