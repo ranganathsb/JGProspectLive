299eb754c41cab89c415537f6be33c4f47e1b144
bhavikvaishnani <bhavikvaishnani@gmail.com>
2016-11-24 15:56:43 +0530

    #ITSN042 - updated USP_SetUserDisplayID for setting InstallUserID

--- JG_Prospect.web/Database Scripts/Bhavik/Script Nov_ITSN042_V2_UpdateUserInstallID.sql
+++ JG_Prospect.web/Database Scripts/Bhavik/Script Nov_ITSN042_V2_UpdateUserInstallID.sql
@@ -1,4 +1,101 @@
 USE [JGBS]
+
+GO
+/****** Object:  StoredProcedure [dbo].[USP_SetUserDisplayID]    Script Date: 11/24/2016 3:53:15 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		Bhavik
+-- Create date: 16 11 2016
+-- Description:	Get Designation ID For a user
+-- =============================================
+ALTER PROCEDURE [dbo].[USP_SetUserDisplayID] 
+	-- Add the parameters for the stored procedure here
+	@InstallUserID int = 0, 
+	@DesignationsCode varchar(15),
+	@UpdateCurrentSequence varchar (10) =''
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+IF (@UpdateCurrentSequence = 'YES')
+BEGIN
+-- SET UserInstallId as NULL. so following process will generae a new ID for respective user.
+
+	UPDATE tblInstallUsers
+		SET UserInstallId = null
+	WHERE Id = @InstallUserID
+END
+
+
+DECLARE @InstallId VARCHAR(50) = NULL
+
+	SELECT @InstallId = UserInstallId
+FROM tblInstallUsers
+WHERE Id = @InstallUserID
+
+IF @InstallId IS NULL
+BEGIN
+	-- get sequence of last entered task for perticular designation.
+	DECLARE @DesSequence bigint
+
+	SELECT @DesSequence = LastSequenceNo FROM tblUserDesigLastSequenceNo WHERE DesignationCode = @DesignationsCode
+
+	-- if it is first time task is entered for designation start from 001.
+	IF(@DesSequence IS NULL)
+	BEGIN
+		SET @DesSequence = 0  
+	END
+
+	SET @DesSequence = @DesSequence + 1  
+	
+
+	UPDATE tblInstallUsers
+		SET UserInstallId = @DesignationsCode + '-A'+ Right('000' + CONVERT(NVARCHAR, @DesSequence), 4)
+	WHERE Id = @InstallUserID
+	
+
+	-- INCREMENT SEQUENCE NUMBER FOR DESIGNATION TO USE NEXT TIME
+	IF NOT EXISTS( 
+					SELECT uds.UserDesigSequenceId 
+					FROM dbo.tblUserDesigLastSequenceNo uds 
+					WHERE uds.DesignationCode = @DesignationsCode 
+				 )
+	BEGIN
+
+	INSERT INTO dbo.tblUserDesigLastSequenceNo
+		(
+    
+			DesignationCode,
+			LastSequenceNo
+		)
+		VALUES
+		(
+			@DesignationsCode,
+			@DesSequence
+		) 
+	END
+	ELSE		
+	BEGIN
+		UPDATE dbo.tblUserDesigLastSequenceNo
+		SET
+			dbo.tblUserDesigLastSequenceNo.LastSequenceNo = @DesSequence
+		WHERE dbo.tblUserDesigLastSequenceNo.DesignationCode = @DesignationsCode 
+	END
+
+	END
+END
+
+
+ 
+
+
+
 GO
 
 TRUNCATE TABLE tblUserDesigLastSequenceNo
