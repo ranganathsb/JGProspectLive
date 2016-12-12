842c5a2cd990bb19a664bce100ddcab7ece76fb5
Jaylem <jaylemchaudhari@hotmail.com>
2016-12-12 15:31:14 +0530

    Task ID#: ITJN019 : Jaylem Chaudhari 2016-12-12 03:30 AM IST

--- JG_Prospect.web/stafflogin.aspx.cs
+++ JG_Prospect.web/stafflogin.aspx.cs
@@ -1018,11 +1018,11 @@ namespace JG_Prospect
                         {
                             if (strRedirectUrl.ToLower().Contains("sr_app") && Request.QueryString["returnurl"].ToLower().Contains("sr_app"))
                             {
-                                strRedirectUrl = Request.QueryString["returnurl"];
+                                strRedirectUrl = HttpUtility.UrlDecode(Request.Url.Query.Replace("?returnurl=", ""));
                             }
                             else if (!strRedirectUrl.ToLower().Contains("sr_app") && !Request.QueryString["returnurl"].ToLower().Contains("sr_app"))
                             {
-                                strRedirectUrl = Request.QueryString["returnurl"];
+                                strRedirectUrl = HttpUtility.UrlDecode(Request.Url.Query.Replace("?returnurl=", ""));
                             }
                         }
                         Response.Redirect(strRedirectUrl);
@@ -1181,15 +1181,21 @@ namespace JG_Prospect
 
         protected void rdUserType_CheckedChanged(object sender, EventArgs e)
         {
+            string returnUrl = string.Empty;
+            if (!string.IsNullOrEmpty(Request.QueryString["returnurl"]))
+            {
+                returnUrl = HttpUtility.UrlDecode(Request.Url.Query);
+            }
+
             RadioButton rb = (RadioButton)sender;
             string pageName = Path.GetFileName(Request.Url.AbsolutePath);
             if (rb.Text == "Customer" && pageName == "stafflogin.aspx")
             {
-                Response.Redirect("login.aspx", false);
+                Response.Redirect("login.aspx" + returnUrl, false);
             }
             if (rb.Text == "Staff" && pageName == "login.aspx")
             {
-                Response.Redirect("stafflogin.aspx", false);
+                Response.Redirect("stafflogin.aspx" + returnUrl, false);
             }
         }
 
