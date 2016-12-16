7323fc485c5657831d30e112bafac2f4c90d9e6c
bhavikvaishnani <bhavikvaishnani@gmail.com>
2016-12-16 10:23:53 +0530

    Updated -- [UDP_GETInstallUserDetails]

--- JG_Prospect.web/Database Scripts/Bhavik/Dec/ITSN042_Dec_P1.sql
+++ JG_Prospect.web/Database Scripts/Bhavik/Dec/ITSN042_Dec_P1.sql
@@ -629,6 +629,7 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
 ALTER PROCEDURE [dbo].[UDP_UpdateInstallUsers]  
 	@id int,  
 	@FristName varchar(50),  
@@ -961,3 +962,30 @@ END
 
 
 SET ANSI_NULLS ON
+
+
+
+/****** Object:  StoredProcedure [dbo].[UDP_GETInstallUserDetails]    Script Date: 12/16/2016 10:14:57 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[UDP_GETInstallUserDetails]
+	@id int
+As 
+BEGIN
+
+	SELECT Id,FristName,Lastname,Email,[Address],Designation,
+	[Status],[Password],Phone,Picture,Attachements,zip,[state],city,
+	Bussinessname,SSN,SSN1,SSN2,[Signature],DOB,Citizenship,' ',
+	EIN1,EIN2,A,B,C,D,E,F,G,H,[5],[6],[7],maritalstatus,PrimeryTradeId,SecondoryTradeId,Source,Notes,StatusReason,GeneralLiability,PCLiscense,WorkerComp,HireDate,TerminitionDate,WorkersCompCode,NextReviewDate,EmpType,LastReviewDate,PayRates,ExtraEarning,ExtraEarningAmt,PayMethod,Deduction,DeductionType,AbaAccountNo,AccountNo,AccountType,PTradeOthers,
+	STradeOthers,DeductionReason,InstallId,SuiteAptRoom,FullTimePosition,ContractorsBuilderOwner,MajorTools,DrugTest,ValidLicense,TruckTools,PrevApply,LicenseStatus,CrimeStatus,StartDate,SalaryReq,Avialability,ResumePath,skillassessmentstatus,assessmentPath,WarrentyPolicy,CirtificationTraining,businessYrs,underPresentComp,websiteaddress,PersonName,PersonType,CompanyPrinciple,UserType,Email2,Phone2,CompanyName,SourceUser,DateSourced,InstallerType,BusinessType,CEO,LegalOfficer,President,Owner,AllParteners,MailingAddress,Warrantyguarantee,WarrantyYrs,MinorityBussiness,WomensEnterprise,InterviewTime,CruntEmployement,CurrentEmoPlace,LeavingReason,CompLit,FELONY,shortterm,LongTerm,BestCandidate,TalentVenue,Boardsites,NonTraditional,ConSalTraning,BestTradeOne,BestTradeTwo,BestTradeThree
+	,aOne,aOneTwo,bOne,cOne,aTwo,aTwoTwo,bTwo,cTwo,aThree,aThreeTwo,bThree,cThree,TC,ExtraIncomeType,RejectionDate ,UserInstallId
+        ,PositionAppliedFor, PhoneExtNo, PhoneISDCode ,DesignationID
+	DesignationID
+	FROM tblInstallUsers 
+	WHERE ID=@id
+
+END
+
+
