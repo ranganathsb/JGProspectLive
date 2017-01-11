f70e99c871ce50cd95087038bd7c5aa75f7f89b6
Yogesh Keraliya <kerconsultancy@hotmail.com>
2017-01-11 11:33:35 +0530

    (task) : email templates.

---
+++ JG_Prospect.BLL/HTMLTemplateBLL.cs
@@ -0,0 +1,38 @@
+﻿using JG_Prospect.Common;
+using JG_Prospect.Common.modal;
+using JG_Prospect.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JG_Prospect.BLL
+{
+    public class HTMLTemplateBLL
+    {
+        private static HTMLTemplateBLL m_HTMLTemplateBLL = new HTMLTemplateBLL();
+
+        private HTMLTemplateBLL()
+        {
+
+        }
+
+        public static HTMLTemplateBLL Instance
+        {
+            get { return m_HTMLTemplateBLL; }
+            set { ; }
+        }
+
+        public DataSet GetHTMLTemplates()
+        {
+            return HTMLTemplateDAL.Instance.GetHTMLTemplates();
+        }
+
+        public HTMLTemplate GetHTMLTemplateByIdAndDesignation(HTMLTemplates objHTMLTemplates, string strDesignation = null)
+        {
+            return HTMLTemplateDAL.Instance.GetHTMLTemplateByIdAndDesignation(objHTMLTemplates, strDesignation);
+        }
+    }
+}
---
+++ JG_Prospect.Common/modal/HTMLTemplate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JG_Prospect.Common.modal
+{
+    public class HTMLTemplate
+    {
+        public int Id { get; set; }
+        public int HTMLTemplatesMaterId { get; set; }
+        public string Designation { get; set; }
+        public string Subject { get; set; }
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public string Footer { get; set; }
+        public DateTime DateUpdated { get; set; }
+    }
+}
