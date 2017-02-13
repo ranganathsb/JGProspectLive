f504f22e49e4dda151dee553d15f7492012cdcc2
Yogesh Keraliya <kerconsultancy@hotmail.com>
2017-02-13 09:23:48 +0530

    (task #60) : recruiter dropdown color code for active and offer made users.

--- JG_Prospect.web/Sr_App/EditUser.aspx.cs
+++ JG_Prospect.web/Sr_App/EditUser.aspx.cs
@@ -3145,7 +3145,7 @@ namespace JG_Prospect
                 {
                     DataRow objUser = dtUsers.Rows[i];
                     ddlUsers.Items.Add(new ListItem(objUser["FristName"].ToString(), objUser["Id"].ToString()));
-                    if (objUser["Status"].ToString() == Convert.ToByte(JGConstant.InstallUserStatus.OfferMade).ToString())
+                    if (objUser["Status"].ToString() == Convert.ToByte(JGConstant.InstallUserStatus.Active).ToString())
                     {
                         ddlUsers.Items[i].Attributes.Add("style", "color: red;");
                     }
