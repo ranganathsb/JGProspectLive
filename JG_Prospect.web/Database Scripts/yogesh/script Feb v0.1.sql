cac9fa8ec32a2255fd5040a9ce69b4ac58de688b
Yogesh Keraliya <kerconsultancy@hotmail.com>
2017-02-16 08:23:12 +0530

    (Task#50) : google calendar view.

--- JG_Prospect.web/Database Scripts/yogesh/script Feb v0.1.sql
+++ JG_Prospect.web/Database Scripts/yogesh/script Feb v0.1.sql
@@ -749,3 +749,110 @@ SET NOCOUNT ON;
 	RETURN;
 END
 GO
+
+/****** Object:  StoredProcedure [dbo].[GetAllAnnualEvent]    Script Date: 16-Feb-17 8:06:11 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dbo].[GetAllAnnualEvent] 
+	-- Add the parameters for the stored procedure here
+	@Year varchar(10)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	--select a.ID,a.EventName,a.EventDate,a.EventAddedBy,a.ApplicantId,u.Username,i.FristName,i.LastName,i.Phone
+	--from tbl_AnnualEvents a INNER JOIN  tblUsers u ON u.Id = a.EventAddedBy LEFT JOIN tblInstallUsers i ON i.Id = a.ApplicantId
+	--where DATEPART(yyyy,EventDate)=@Year
+
+	SELECT 
+		a.ID,a.EventName,a.EventDate,a.EventAddedBy,a.ApplicantId,u.Username,i.FristName,i.LastName,i.Phone,i.Id,
+		i.Status, i.Designation, i.Email, Task.TaskId , ISNULL(Task.InstallId,'') AS InstallId ,
+		STUFF
+		(
+			(SELECT  CAST(', ' + u.FristName as VARCHAR) AS Name
+			FROM tbl_AnnualEventAssignedUsers tu
+				INNER JOIN tblInstallUsers u ON tu.UserId = u.Id
+			WHERE tu.EventId = a.Id
+			FOR XML PATH(''), TYPE).value('.','NVARCHAR(MAX)')
+			,1
+			,2
+			,' '
+		) AS AssignedUserFristNames
+	FROM 
+		tbl_AnnualEvents a 
+		OUTER APPLY 
+		(
+			SELECT TOP 1 Id, Username, FirstName AS FristName
+			FROM tblUsers
+			WHERE tblUsers.id = a.EventAddedBy AND ISNULL(a.IsInstallUser,0) = 0
+
+			UNION
+
+			SELECT TOP 1  Id, Email AS Username, FristName
+			FROM tblInstallUsers
+			WHERE tblInstallUsers.id = a.EventAddedBy AND ISNULL(a.IsInstallUser,1) = 1
+		) u --INNER JOIN  tblUsers u ON u.Id = a.EventAddedBy 
+		LEFT JOIN tblInstallUsers i ON i.Id = a.ApplicantId 
+		OUTER APPLY
+		(
+			SELECT TOP 1 tTask.TaskId, tTask.InstallId, ROW_NUMBER() OVER (ORDER BY TaskAss.TaskUserId DESC) AS Row_No
+			FROM tblTaskAssignedUsers TaskAss
+					LEFT JOIN tblTask tTask ON tTask.TaskId = TaskAss.TaskId
+			WHERE TaskAss.UserId = i.Id
+		) Task
+	WHERE 
+		a.EventName IS NOT NULL AND 
+		a.EventName !='InterViewDetails' AND 
+		DATEPART(yyyy,EventDate)=@Year
+	
+	UNION 
+
+	SELECT 
+		a.ID,( a.EventName + '  '+u.Username + ' '+i.Designation+  '  ' + i.Phone +'   '+i.FristName) as EventName,
+		EventDate = CONVERT(datetime,EventDate) + CONVERT(datetime,InterviewTime),a.EventAddedBy,a.ApplicantId,u.Username,i.FristName,i.LastName,i.Phone,i.Id,
+		i.Status, i.Designation, i.Email, Task.TaskId , ISNULL(Task.InstallId,'') AS InstallId,
+		STUFF
+		(
+			(SELECT  CAST(', ' + u.FristName as VARCHAR) AS Name
+			FROM tbl_AnnualEventAssignedUsers tu
+				INNER JOIN tblInstallUsers u ON tu.UserId = u.Id
+			WHERE tu.EventId = a.Id
+			FOR XML PATH(''), TYPE).value('.','NVARCHAR(MAX)')
+			,1
+			,2
+			,' '
+		) AS AssignedUserFristNames
+	FROM 
+		tbl_AnnualEvents a 
+		OUTER APPLY 
+		(
+			SELECT TOP 1 Id, Username, FirstName AS FristName
+			FROM tblUsers
+			WHERE tblUsers.id = a.EventAddedBy AND ISNULL(a.IsInstallUser,0) = 0
+
+			UNION
+
+			SELECT TOP 1  Id, Email AS Username, FristName
+			FROM tblInstallUsers
+			WHERE tblInstallUsers.id = a.EventAddedBy AND ISNULL(a.IsInstallUser,1) = 1
+		) u --INNER JOIN  tblUsers u ON u.Id = a.EventAddedBy 
+		LEFT JOIN tblInstallUsers i ON i.Id = a.ApplicantId
+		OUTER APPLY
+		(
+			SELECT TOP 1 tTask.TaskId, tTask.InstallId, ROW_NUMBER() OVER (ORDER BY TaskAss.TaskUserId DESC) AS Row_No
+			FROM tblTaskAssignedUsers TaskAss
+					LEFT JOIN tblTask tTask ON tTask.TaskId = TaskAss.TaskId
+			WHERE TaskAss.UserId = i.Id
+		) Task
+	WHERE 
+		a.EventName='InterViewDetails' AND 
+		DATEPART(yyyy,EventDate)=@Year
+
+END
+GO
