1d587a313b2d3f636cb8ed7af1b77f727853df78
Yogesh Keraliya <kerconsultancy@hotmail.com>
2017-04-12 11:54:04 +0530

    (Task) : added tables, stored procedures, dal, bll for task comment.

---
+++ JG_Prospect.BLL/TaskCommentBLL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JG_Prospect.BLL
+{
+    public class TaskCommentBLL
+    {
+        private static TaskCommentBLL m_TaskCommentBLL = new TaskCommentBLL();
+
+        public static TaskCommentBLL Instance
+        {
+            get { return m_TaskCommentBLL; }
+            set {; }
+        }
+    }
+}
---
+++ JG_Prospect.DAL/TaskCommentDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JG_Prospect.DAL
+{
+    public class TaskCommentDAL
+    {
+        private static TaskCommentDAL m_TaskCommentDAL = new TaskCommentDAL();
+        public static TaskCommentDAL Instance
+        {
+            get { return m_TaskCommentDAL; }
+            private set { ; }
+        }
+
+    }
+}
