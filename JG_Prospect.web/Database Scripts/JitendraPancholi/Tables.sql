009c6b51c1b1d2cb80bdf86caa2b318ce31aba0e
Jitendra Pancholi <pancholi.jitu@hotmail.com>
2017-11-10 00:11:04 +0530

    Jitendra Pancholi - success popup - re sequence algorithm(DB Scripts Added) - ITSN052-XI-XI - a

---
+++ JG_Prospect.web/Database Scripts/JitendraPancholi/Tables.sql
@@ -0,0 +1,10 @@
+﻿IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'JobSchedulerLog') AND type in (N'U'))
+BEGIN
+	CREATE TABLE JobSchedulerLog(
+		Id Bigint Primary Key Identity(1,1),
+		JobName Varchar(200) Not Null,
+		StartsOn DateTime Not Null Default(GetDate()),
+		EndsOn DateTime,
+		ExecutionTime int
+	)
+END
